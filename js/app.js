a6195ae0031f7f8641c04d67b63a905aad2216fb
Gray-Eagle <kamkamij@gmail.com>
2017-06-28 01:10:15 +0300

    Почти все!

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('.index_block').height($(window).height() - $('.header_block').height());
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQSIsImZpbGUiOiJhcHAuanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbiAoKSB7XHJcbiAgICAkKCdbZGF0YS10b2dnbGU9XCJ0b29sdGlwXCJdJykudG9vbHRpcCgpO1xyXG4gICAgJCgnLmluZGV4X2Jsb2NrJykuaGVpZ2h0KCQod2luZG93KS5oZWlnaHQoKSAtICQoJy5oZWFkZXJfYmxvY2snKS5oZWlnaHQoKSk7XHJcbn0pOyJdfQ==
